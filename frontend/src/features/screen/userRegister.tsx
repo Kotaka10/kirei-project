8412491b4b76c8f1b169fa1d0fa34f09c85ef6b5
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-09 15:48:03 +0900

    refactor: フォーム周りの背景の色変更

--- frontend/src/features/screen/userRegister.tsx
+++ frontend/src/features/screen/userRegister.tsx
@@ -7,70 +7,72 @@ export default function UserRegister() {
     return (
         <>
             <h1 className="text-3xl text-center p-6">ユーザー登録</h1>
-            <form className="bg-green-100 p-10 m-auto max-w-md rounded-xl flex flex-col items-center justify-center gap-8">
-                <div className="flex flex-col w-64 sm:w-72 md:w-96">
-                    <p className="mb-2">名前</p>
-                    <input
-                        type="text"
-                        name="userName"
-                        className="bg-white p-2 ring-1 ring-black rounded-md"
-                    />
+            <form className="bg-green-100 p-8 m-auto max-w-lg rounded-xl">
+                <div className="bg-white p-4 rounded-xl flex flex-col items-center justify-center gap-8">
+                    <div className="flex flex-col w-64 sm:w-72 md:w-96">
+                        <p className="mb-2">名前</p>
+                        <input
+                            type="text"
+                            name="userName"
+                            className="bg-white p-2 ring-1 ring-black rounded-md"
+                        />
+                    </div>
+                    <div className="flex flex-col w-64 sm:w-72 md:w-96">
+                        <p className="mb-2">TEL</p>
+                        <input
+                            type="text"
+                            name="phoneNumber"
+                            className="bg-white p-2 ring-1 ring-black rounded-md"
+                        />
+                    </div>
+                    <div className="flex flex-col w-64 sm:w-72 md:w-96">
+                        <p className="mb-2">住所</p>
+                        <input
+                            type="text"
+                            name="address"
+                            className="bg-white p-2 ring-1 ring-black rounded-md"
+                        />
+                    </div>
+                    <div className="flex flex-col w-64 sm:w-72 md:w-96">
+                        <p className="mb-2">発行日</p>
+                        <input
+                            type="text"
+                            name="publicationDate"
+                            className="bg-white p-2 ring-1 ring-black rounded-md"
+                        />
+                    </div>
+                    <div className="flex flex-col w-64 sm:w-72 md:w-96">
+                        <p className="mb-2">有効期限</p>
+                        <input
+                            type="text"
+                            name="expirationDate"
+                            className="bg-white p-2 ring-1 ring-black rounded-md"
+                        />
+                    </div>
+                    <div className="flex flex-col w-64 sm:w-72 md:w-96">
+                        <p className="mb-2">備考欄</p>
+                        <input
+                            type="text"
+                            name="notes"
+                            className="bg-white p-2 ring-1 ring-black rounded-md"
+                        />
+                    </div>
+                    <div className="flex flex-col w-64 sm:w-72 md:w-96">
+                        <p className="mb-2">案件メモ</p>
+                        <input
+                            type="text"
+                            name="memo"
+                            className="bg-white p-2 ring-1 ring-black rounded-md"
+                        />
+                    </div>
+                    <Link
+                        to="/item-register"
+                        className="text-blue-500 underline hover:text-blue-700"
+                    >
+                        商品登録が済んでない方はこちら
+                    </Link>
                 </div>
-                <div className="flex flex-col w-64 sm:w-72 md:w-96">
-                    <p className="mb-2">TEL</p>
-                    <input
-                        type="text"
-                        name="phoneNumber"
-                        className="bg-white p-2 ring-1 ring-black rounded-md"
-                    />
-                </div>
-                <div className="flex flex-col w-64 sm:w-72 md:w-96">
-                    <p className="mb-2">住所</p>
-                    <input
-                        type="text"
-                        name="address"
-                        className="bg-white p-2 ring-1 ring-black rounded-md"
-                    />
-                </div>
-                <div className="flex flex-col w-64 sm:w-72 md:w-96">
-                    <p className="mb-2">発行日</p>
-                    <input
-                        type="text"
-                        name="publicationDate"
-                        className="bg-white p-2 ring-1 ring-black rounded-md"
-                    />
-                </div>
-                <div className="flex flex-col w-64 sm:w-72 md:w-96">
-                    <p className="mb-2">有効期限</p>
-                    <input
-                        type="text"
-                        name="expirationDate"
-                        className="bg-white p-2 ring-1 ring-black rounded-md"
-                    />
-                </div>
-                <div className="flex flex-col w-64 sm:w-72 md:w-96">
-                    <p className="mb-2">備考欄</p>
-                    <input
-                        type="text"
-                        name="notes"
-                        className="bg-white p-2 ring-1 ring-black rounded-md"
-                    />
-                </div>
-                <div className="flex flex-col w-64 sm:w-72 md:w-96">
-                    <p className="mb-2">案件メモ</p>
-                    <input
-                        type="text"
-                        name="memo"
-                        className="bg-white p-2 ring-1 ring-black rounded-md"
-                    />
-                </div>
-                <Link
-                    to="/item-register"
-                    className="text-blue-500 underline hover:text-blue-700"
-                >
-                    商品登録ページ
-                </Link>
-            </form>
+            </form>    
         </>
     )
 }
