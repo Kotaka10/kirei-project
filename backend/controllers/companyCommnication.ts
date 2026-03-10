6df6011c7c8611adbe77f29d63c0c82af835f1bf
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-10 15:22:10 +0900

    feat: server.tsからのapi処理のコードを追加

---
+++ backend/controllers/companyCommnication.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+const companies = require("./data/companies.json");
+const fs = require("fs");
+
+type Params = { id: string};
+
+app.get("/api/companies/:id", (req: Request<Params>, res: Response) => {
+    const id = req.params.id;
+    const company = companies.find((c: { id: string; }) => c.id == id);
+
+    if (!company) {
+        return res.status(404).json({ message: "ページが見つかりません" });
+    }
+    
+    res.json(company);
+});
+
+app.get("/api/companies", (req: Request, res: Response) => {
+    res.json(companies);
+})
+
+app.put("/api/companies/:id", (req: Request<Params>, res: Response) => {
+    const id = Number(req.params.id);
+    const updatedData = req.body;
+
+    const index = companies.findIndex((c: { id: any; }) => Number(c.id) === id);
+
+    if (index === -1) {
+        return res.status(404).json({ message: "会社が見つかりません" });
+    }
+
+    companies[index] = { ...companies[index], ...updatedData };
+
+    fs.writeFileSync(
+        "./companies.json",
+        JSON.stringify(companies, null, 2)
+    );
+
+    res.json(companies[index]);
+});
