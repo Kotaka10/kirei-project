9b0a2c8633210190855b1396826bb05e72171437
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-10 09:42:08 +0900

    feat: ユーザー情報の型のファイルを追加

---
+++ frontend/src/features/screen/types/userInfoTypes.ts
@@ -0,0 +1,13 @@
+export type userInfoTypes = {
+    name: string;
+    phoneNumber: string;
+    zipcode: string;
+    prefetures: string;
+    cities: string;
+    otherAddress: string;
+    buildingName: string;
+    publicationDate: string;
+    expirationDate: string;
+    notes: string;
+    memo: string;
+};
