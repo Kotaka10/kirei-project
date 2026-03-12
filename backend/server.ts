ed631274836dbe2ca85c1ce0e6a97835101553e7
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-12 10:22:12 +0900

    chore: ESModulesの書き方に変更

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+import express from "express";
+import cors from "cors";
+import companyRoutes from "./routes/companyRoutes.ts";
 
-const useRoutes = require("./controllers/companyCommunication.ts");
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/companies", useRoutes);
+app.use("/api/companies", companyRoutes);
 
 app.listen(3000, () => {
     console.log("server running on port 3000");
