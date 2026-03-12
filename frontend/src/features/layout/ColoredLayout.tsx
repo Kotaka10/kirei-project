71b81c7daf78fa0b882b1e9ef33f04629deee65e
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-12 15:42:32 +0900

    refactor: 画面上部の背景色を変更するファイル追加

---
+++ frontend/src/features/layout/ColoredLayout.tsx
@@ -0,0 +1,17 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function ColoredLayout() {
+  return (
+    <div className="min-h-screen bg-slate-100">
+       <nav className="flex items-center justify-center gap-4">
+        <Link to="/register">会社登録ページ</Link>
+        <Link to="/list">会社一覧</Link>
+        <Link to="/user-register">ユーザー登録ページ</Link>
+        <Link to="/item-register">商品登録ページ</Link>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
