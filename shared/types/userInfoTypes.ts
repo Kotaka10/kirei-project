b3adb33cf386dc26074d299417d58d31829e99f5
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-13 16:26:33 +0900

    refactor: ファイル移動

---
+++ shared/types/userInfoTypes.ts
@@ -0,0 +1,14 @@
+export type userInfoTypes = {
+    id: number;
+    name: string;
+    phoneNumber: string;
+    zipcode: string;
+    prefecture: string;
+    city: string;
+    otherAddress: string;
+    buildingName: string;
+    publicationDate: string;
+    expirationDate: string;
+    notes: string;
+    memo: string;
+};
