f05765e6d90339bf6285e56cd21f69c67247622e
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-13 12:02:58 +0900

    refactor: ロジックの部分をカスタムフックに変更

--- frontend/src/features/user/components/UserRegister.tsx
+++ frontend/src/features/user/components/UserRegister.tsx
@@ -1,56 +1,12 @@
 import { Link } from "react-router-dom";
-import type { userInfoTypes } from "../types/userInfoTypes";
-import { useState } from "react";
+import useUserRegister from "./hooks/useUserRegister"
 
 export default function UserRegister() {
-    const [form, setForm] = useState<userInfoTypes>({
-        name: "",
-        phoneNumber: "",
-        zipcode: "",
-        prefecture: "",
-        city: "",
-        otherAddress: "",
-        buildingName: "",
-        publicationDate: "",
-        expirationDate: "",
-        notes: "",
-        memo: "",
-    });
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const name = e.target.name as keyof userInfoTypes;
-
-        setForm((prev: userInfoTypes) => ({
-            ...prev,
-            [name]: e.target.value,
-        }))
-    }
-    
-    const handleFetchAddress = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        fetchAddress(form.zipcode);
-    }
-
-    const fetchAddress = async (zipcode: string) => {
-        if (!/^\d{7}$/.test(zipcode)) {
-            alert("有効な7桁の数字を入力してください。")
-            return;
-        }
-
-        try {
-            const res = await fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipcode}`);
-            const data = await res.json();
-            if (data.results) {
-                setForm((prev) => ({
-                    ...prev,
-                    prefecture: data.results[0].address1,
-                    city: data.results[0].address2 + data.results[0].address3,
-                }))
-            }
-        } catch (error) {
-            alert("住所取得に失敗しました。")
-        }
-    }
+    const {
+        form,
+        handleChange,
+        handleFetchAddress,
+    } = useUserRegister();
 
     return (
         <>
