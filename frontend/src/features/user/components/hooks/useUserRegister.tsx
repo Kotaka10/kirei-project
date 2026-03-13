190948c242ee4bbc098f759d49f5e408daf1ab68
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-13 12:03:48 +0900

    refactor: カスタムフックとしてロジック部分を別ファイルから移動

---
+++ frontend/src/features/user/components/hooks/useUserRegister.tsx
@@ -0,0 +1,59 @@
+import type { userInfoTypes } from "../../types/userInfoTypes";
+import { useState } from "react";
+
+export default function useUserRegister() {
+    const [form, setForm] = useState<userInfoTypes>({
+        name: "",
+        phoneNumber: "",
+        zipcode: "",
+        prefecture: "",
+        city: "",
+        otherAddress: "",
+        buildingName: "",
+        publicationDate: "",
+        expirationDate: "",
+        notes: "",
+        memo: "",
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const name = e.target.name as keyof userInfoTypes;
+
+        setForm((prev: userInfoTypes) => ({
+            ...prev,
+            [name]: e.target.value,
+        }))
+    }
+    
+    const handleFetchAddress = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        fetchAddress(form.zipcode);
+    }
+
+    const fetchAddress = async (zipcode: string) => {
+        if (!/^\d{7}$/.test(zipcode)) {
+            alert("有効な7桁の数字を入力してください。")
+            return;
+        }
+
+        try {
+            const res = await fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipcode}`);
+            const data = await res.json();
+            if (data.results) {
+                setForm((prev) => ({
+                    ...prev,
+                    prefecture: data.results[0].address1,
+                    city: data.results[0].address2 + data.results[0].address3,
+                }))
+            }
+        } catch (error) {
+            alert("住所取得に失敗しました。")
+        }
+    }
+
+    return {
+        form,
+        handleChange,
+        handleFetchAddress,
+    };
+}
