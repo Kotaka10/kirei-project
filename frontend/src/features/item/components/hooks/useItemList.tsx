df7446121f351f3e01f86b92faa6f51f1863638a
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-16 14:04:13 +0900

    feat: 初回レンダリング時に商品情報を取得できるようuseeffectを追加

--- frontend/src/features/item/components/hooks/useItemList.tsx
+++ frontend/src/features/item/components/hooks/useItemList.tsx
@@ -1,12 +1,25 @@
-
+import { useEffect, useState } from "react";
 
 export default function userItemList() {
+    const initialItems= [{
+        id: 0,
+        itemName: "",
+        content: "",
+        quantity: "",
+        unit: "",
+        unitPrice: "",
+        price: "",
+    }];
+
+    const [msg, setMsg] = useState("");
+    const [items, setItems] = useState(initialItems);
+
     const handleFindItem = async (e: React.ChangeEvent<HTMLInputElement>) => {
         const input = e.target.value;
 
         if (!input) return [];
 
-        const res = await fetch("http://localhost:3000/api/item");
+        const res = await fetch("http://localhost:3000/api/items");
         const data = await res.json();
 
         const results = data.filter((item: { itemName: string }) =>
@@ -14,5 +27,35 @@ export default function userItemList() {
         );
 
         return results;
+    }
+
+    useEffect(() => {
+        const handelFetchItems = async () => {
+            try {
+                const res = await fetch('http://localhost:3000/api/items');
+
+                if (!res.ok) {
+                    console.error("商品情報の読み込みに失敗しました。")
+                }
+
+                const data = await res.json();
+                setItems(data);
+                setMsg("商品情報の取得に成功しました。");
+            } catch (error) {
+                if (error instanceof Error) {
+                    setMsg(error.message);
+                } else {
+                    setMsg("商品情報の取得に失敗しました。");
+                }
+            }
+        }
+
+        handelFetchItems();
+    }, []);
+
+    return {
+        items,
+        msg,
+        handleFindItem
     }
 }
