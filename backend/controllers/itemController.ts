fba539f6bcad3761e236929d920a7d7e6311268b
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-16 10:56:29 +0900

    refactor: inportするファイルの呼び出し時の文言変更(itemServicee → itemservice)

--- backend/controllers/itemController.ts
+++ backend/controllers/itemController.ts
@@ -1,9 +1,9 @@
 import type { Request, Response } from "express";
-import * as itemServicee from "../services/itemService.js";
+import * as itemService from "../services/itemService.js";
 
 export const addItem = (req: Request, res: Response) => {
     const item = req.body;
-    const newItem = itemServicee.addItem(item);
+    const newItem = itemService.addItem(item);
 
     res.json(newItem);
 }
