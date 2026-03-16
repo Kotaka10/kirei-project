7d787a6c9dd64cd34a5afdd32edb61323baaf937
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-16 16:41:27 +0900

    feat: 検索のルーター追加

--- backend/routes/itemRoutes.ts
+++ backend/routes/itemRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { addItem, getItems } from "../controllers/itemController.js";
+import { searchItem } from "../controllers/itemController.js";
 
 const router = express.Router();
 
 router.post("/", addItem);
 router.get("/", getItems);
+router.get("/search", searchItem);
 
 export default router;
