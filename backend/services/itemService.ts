599acf8f362e2ace78e3e211678155b9a3867ea8
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-19 18:04:40 +0900

    feat: JSONデータからDBデータの取り扱いに変更

--- backend/services/itemService.ts
+++ backend/services/itemService.ts
@@ -1,79 +1,126 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 import type { itemInfoTypes } from "../../shared/types/itemInfoTypes.js";
-import items from "../data/items.json" with {type: "json"};
+import mysql from "mysql2/promise";
+import type { ResultSetHeader, RowDataPacket } from "mysql2/promise";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const dataPath = path.join(__dirname, "../data/items.json");
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "user",
+    password: "password",
+    database: "kirei_db",
+    waitForConnections: true,
+    connectionLimit: 10,
+});
 
-export const getAllItems = () => {
-    return JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+export const getItems = async () => {
+    const [rows] = await pool.query<RowDataPacket[]>(`
+        SELECT id, item_name, quantity, unit_price, description, unit
+        FROM items
+        ORDER BY id DESC
+    `);
+
+    const items = rows.map((row: any) => ({
+        id: row.id,
+        itemName: row.item_name,
+        description: row.description,
+        quantity: row.quantity,
+        unit: row.unit,
+        unitPrice: row.unit_price
+    }));
+
+    return items;
 }
 
-export const addItem = (item: itemInfoTypes) => {
-    const items = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+export const addItem = async (item: itemInfoTypes) => {
+    const [result] = await pool.query<ResultSetHeader>(
+        `
+            INSERT INTO items (
+                item_name,
+                quantity,
+                unit_price,
+                description,
+                unit
+            ) VALUES (?, ?, ?, ?, ?)
+        `,
+        [
+            item.itemName,
+            item.quantity,
+            item.unitPrice,
+            item.description,
+            item.unit
+        ]
+    );
 
-    const maxId = items.length
-        ? Math.max(...items.map((i: itemInfoTypes) => i.id))
-        : 0
-    ;
+
 
     const newItem = {
         ...item,
-        id: maxId + 1,
-    };
-
-    items.push(newItem);
-
-    fs.writeFileSync(
-        dataPath,
-        JSON.stringify(items, null, 2)
-    );
+        id: result.insertId,
+    }
 
     return newItem;
 }
 
-export const searchItem = (keyword: string) => {
-    return items.filter((item) => 
-        item.itemName.toLowerCase().includes(keyword.trim().toLowerCase())
+export const searchItem = async (keyword: string) => {
+    const [rows] = await pool.query<RowDataPacket[]>(
+        `
+            SELECT id, item_name, quantity, unit_price, description, unit
+            FROM items
+            WHERE item_name LIKE ?
+        `,
+        [`%${keyword}`]
     );
+
+    return rows.map((row) => ({
+        id: row.id,
+        itemName: row.item_name,
+        quanity: row.quantity,
+        unitPrice: row.unit_price,
+        description: row.description,
+        unit: row.unit,
+    }));
 }
 
-export const updateItem = (id: number, item: itemInfoTypes) => {
-    const items = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
-    const index = items.findIndex((i: {id: number}) => i.id === id);
+export const updateItem = async (id: number, item: itemInfoTypes) => {
+    const [result] = await pool.query<ResultSetHeader>(
+        `
+            UPDATE items
+            SET
+                item_name = ?,
+                quantity = ?,
+                unit_price = ?,
+                description = ?,
+                unit = ?
+            WHERE id = ? 
+        `,
+        [
+            item.itemName,
+            item.quantity,
+            item.unitPrice,
+            item.description,
+            item.unit,
+            id,
+        ]
+    );
 
-    if (index === -1) {
+    if (result.affectedRows === 0) {
         return null;
     }
 
-    items[index] = {
-        ...items[index],
-        ...item
-    }
-
-    fs.writeFileSync(
-        dataPath,
-        JSON.stringify(items, null, 2)
-    )
-
-    return items[index];
+    return { ...item, id };
 }
 
-export const deleteItem = (id: number) => {
-    const items = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
-    const index = items.findIndex((i: itemInfoTypes) => i.id === id);
+export const deleteItem = async (id: number) => {
+    const [result] = await pool.query<ResultSetHeader>(
+        `
+            DELETE FROM items
+             WHERE id = ?
+        `,
+        [id]
+    );
 
-    if (index === -1) {
+    if (result.affectedRows === 0) {
         return false;
     }
 
-    items.splice(index, 1);
-
-    fs.writeFileSync(
-        dataPath,
-        JSON.stringify(items, null, 2)
-    )
     return true;
 }
