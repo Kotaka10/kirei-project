d564cd80eaae6cf736bea5d6baa140cbe5dda895
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-24 11:33:19 +0900

    refactor: async/awaitの削除

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,9 +1,9 @@
 import type { Request, Response } from "express";
 import * as userService from "../services/userService.js";
 
-export const createUser = (req: Request, res: Response) => {
+export const createUser = async (req: Request, res: Response) => {
     const newData = req.body;
-    const newUser = userService.createUser(newData);
+    const newUser = await userService.createUser(newData);
 
     res.json(newUser);
 }
