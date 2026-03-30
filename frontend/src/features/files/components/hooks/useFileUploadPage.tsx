37cb7152ffaa9ab098f647f6a2160cbe7645ee4a
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-30 17:29:10 +0900

    feat: ファイルアップロードのフック追加

---
+++ frontend/src/features/files/components/hooks/useFileUploadPage.tsx
@@ -0,0 +1,90 @@
+import  { useEffect, useState } from "react";
+import type { FileInfoTypes } from "../../../../../../shared/types/FileInfoTypes";
+
+export default function useFileUploadPage() {
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [files, setFiles] = useState<FileInfoTypes[]>([]);
+    const [message, setMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.files || e.target.files.length === 0) {
+            setSelectedFile(null);
+            return;
+        }
+        setSelectedFile(e.target.files[0]);
+    }
+
+    const fetchFiles = async () => {
+        try {
+            const res = await fetch("http://localhost/api/files");
+            if (!res.ok) {
+                throw new Error("ファイルの取得に失敗しました");
+            }
+            const data = await res.json();
+            setFiles(data);
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error(error);
+                setMessage(error.message);
+            } else {
+                console.error("通信の問題が発生しました");
+                setMessage("通信の問題が発生しました");
+            }
+        }
+    }
+
+    const handleUpload = async () => {
+        if (!selectedFile) {
+            setMessage("ファイルを選択して下さい");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("file", selectedFile);
+
+        try {
+            setIsLoading(true);
+            setMessage("");
+
+            const res = await fetch("http://localhost:3000/api/files/upload", {
+                method: "POST", 
+                body: formData,
+            });
+
+            if (!res.ok) {
+                const errorData = await res.json();
+                throw new Error(errorData.message || "アップロードに失敗しました");
+            }
+
+            const data = await res.json();
+            setMessage(`アップロード成功： ${data.fileName}`);
+            setSelectedFile(null);
+
+            await fetchFiles();
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error(error);
+                setMessage(error.message);
+            } else {
+                console.error("通信の問題が発生しました");
+                setMessage("通信の問題が発生しました");
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchFiles();
+    }, []);
+
+    return {
+        selectedFile,
+        files,
+        message,
+        isLoading,
+        handleFileChange,
+        handleUpload
+    }
+}
