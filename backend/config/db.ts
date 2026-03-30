97c4cc79368a8bec5d15f56f1ea5a70b945a802e
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-30 13:05:12 +0900

    rafactor: メモの内容変更

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,6 +1,6 @@
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
+const pool = mysql.createPool({ // プール：DBの接続管理をする箱
     host: "db",
     user: "root",
     password: process.env.DB_PASSWORD || "",
