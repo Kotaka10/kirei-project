0d45d65d82b10e1871d5014073ac175a7d278286
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-31 18:24:32 +0900

    feat: ファイル追加の関数追加

--- backend/services/fileService.ts
+++ backend/services/fileService.ts
@@ -1,3 +1,4 @@
+import type { FileInfoTypes } from "../../shared/types/FileInfoTypes.js";
 import { fileRepository } from "../repositories/fileRepository.js";
 
 export const fileService = {
@@ -12,11 +13,21 @@ export const fileService = {
             size: file.size
         };
 
-        await fileRepository.insert(fileInfo)
+        const insertId = await fileRepository.insert(fileInfo)
 
         return {
+            id: insertId,
             ...fileInfo,
-            url: `http://localhost:8080/${fileInfo.storedPath}`
+            url: `http://localhost:3000/${fileInfo.storedPath}`
         };
+    },
+
+    async getAllFiles() {
+        const files = await fileRepository.findAllFiles();
+
+        return (files as FileInfoTypes[]).map((file) => ({
+            ...file,
+            url: `http://localhost:3000/${file.storedPath}`
+        }));
     }
 };
