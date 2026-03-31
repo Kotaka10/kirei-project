c1634599e56cdd62b7c24a8ddd718851b5c8d181
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-03-31 18:26:26 +0900

    refactor: 型の定義変更

--- shared/types/FileInfoTypes.ts
+++ shared/types/FileInfoTypes.ts
@@ -1,7 +1,9 @@
 export type FileInfoTypes = {
+    id: number;
     fileName: string;
     storedFileName: string;
     storedPath: string;
     mimeType: string; // ファイルタイプ（image/pngなど）
     size: number;
+    url: string;
 };
