d9d81ac0a99558d156cec4d8714b99e641a713c7
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-04-08 11:48:46 +0900

    refactor: 重複しているコードを共通化

--- backend/services/itemService.ts
+++ backend/services/itemService.ts
@@ -1,23 +1,28 @@
 import type { ItemInfoTypes } from "../../shared/types/ItemInfoTypes.js";
+import type { ItemRowTypes } from "../types/ItemRowTypes.js";
 import * as itemRepository from "../repositories/itemRepository.js";
+
+const formatItem = (item: ItemRowTypes) => ({
+    id: item.id,
+    itemName: item.item_name,
+    quantity: item.quantity,
+    unit: item.unit,
+    unitPrice: item.unit_price,
+    description: item.description
+});
 
 export const getItems = async () => {
     const [rows] = await itemRepository.getItems();
 
-    const items = rows.map((row: any) => ({
-        id: row.id,
-        itemName: row.item_name,
-        description: row.description,
-        quantity: row.quantity,
-        unit: row.unit,
-        unitPrice: row.unit_price
-    }));
-
-    return items;
+    return (rows as ItemRowTypes[]).map(formatItem);
 }
 
 export const addItem = async (item: ItemInfoTypes) => {
     const id = await itemRepository.addItem(item);
+
+    if (!id) {
+        throw new Error("正しいidを取得できませんでした");
+    }
 
     return {
         ...item,
@@ -28,18 +33,15 @@ export const addItem = async (item: ItemInfoTypes) => {
 export const searchItem = async (keyword: string) => {
     const [rows] = await itemRepository.searchItem(keyword);
 
-    return rows.map((row) => ({
-        id: row.id,
-        itemName: row.item_name,
-        quantity: row.quantity,
-        unitPrice: row.unit_price,
-        description: row.description,
-        unit: row.unit,
-    }));
+    return (rows as ItemRowTypes[]).map(formatItem);
 }
 
 export const updateItem = async (id: number, item: ItemInfoTypes) => {
-    return await itemRepository.updateItem(id, item);
+    const updated =  await itemRepository.updateItem(id, item);
+
+    if (!updated) return null;
+
+    return updated;
 }
 
 export const deleteItem = async (id: number) => {
