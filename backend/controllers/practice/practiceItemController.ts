ee58b222daf152ef5ea89ae1e84f7ae303d92bf6
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-04-08 11:55:17 +0900

    refactor: 練習ファイルのコーディング変更

--- backend/controllers/practice/practiceItemController.ts
+++ backend/controllers/practice/practiceItemController.ts
@@ -3,76 +3,87 @@ import * as itemService from "../../services/itemService.js";
 
 export const addItem = async (req: Request, res: Response) => {
     try {
-        const newItem = req.body;
-        const addedItem = await itemService.addItem(newItem);
+        const item = req.body;
+        const newItem = await itemService.addItem(item);
 
-        res.json(addedItem);
+        if (!newItem) {
+            return res.status(400).json({ message: "商品を登録できませんでした" });
+        }
+
+        return res.status(201).json(newItem);
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: "商品の登録に失敗しました" });
+        return res.status(500).json({ message: "商品の登録に失敗しました" });
     }
 }
 
 export const getItems = async (req: Request, res: Response) => {
     try {
         const items = await itemService.getItems();
-        res.status(200).json(items);
+
+        return res.status(200).json(items);
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: "商品に取得に失敗しました"});
+        return res.status(500).json({ message: "商品の取得に失敗しました" });
     }
 }
 
 export const searchItem = async (req: Request, res: Response) => {
     try {
-        const name = req.query.name as string;
+        const name = req.query.name;
 
         if (!name) {
-            res.status(400).json({ message: "名前の情報が必要です"});
+            return res.status(400).json({ message: "商品名が必要です" });
         }
 
-        const items = await itemService.searchItem(name);
-        res.json(items);
+        const items = await itemService.searchItem(name as string);
+
+        if (!items) {
+            return res.status(404).json({ message: "商品が見つかりません" });
+        }
+
+        return res.status(200).json(items);
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: "商品の検索に失敗しました"});
+        return res.status(500).json({ message: "商品の取得に失敗しました" });
     }
 }
 
 export const updateItem = async (req: Request, res: Response) => {
     try {
-        const id = req.params.id;
+        const id = Number(req.params.id);
         const item = req.body;
-        const updatedItem = await itemService.updateItem(Number(id),item);
 
-        if (!updateItem) {
-            res.status(404).json({ message: "商品が見つかりませn" });
+        if (Number.isNaN(id)) {
+            return res.status(400).json({ message: "正しいidが必要です" });
         }
 
-        res.json({ message: "更新しました", updatedItem });
+        const updatedItem = await itemService.updateItem(id, item);
+
+        if (!updatedItem) {
+            return res.status(404).json({ message: "商品が見つかりません" });
+        }
+
+        return res.status(200).json(updatedItem);
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: "更新に失敗しました"});
+        return res.status(500).json({ message: "商品の更新に失敗しました" });
     }
 }
 
 export const deleteItem = async (req: Request, res: Response) => {
     try {
-        const id = req.params.id;
+        const id = Number(req.params.id);
 
-        if (!id) {
-            res.status(400).json({ message: "idが必要です" });
+        if (Number.isNaN(id)) {
+            return res.status(400).json({ message: "正しいidが必要です" });
         }
 
-        const result = await itemService.deleteItem(Number(id));
-
-        if (!result) {
-            res.status(404).json({ message: "商品が見つかりません" });
-        }
-
-        res.json({ message: "削除しました ", result });
+        const deleted = await itemService.deleteItem(id);
+        
+        return res.status(200).json({ message: "商品の削除に成功しました", deleted});
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: "商品の削除に失敗しました" });
+        return res.status(500).json({ message: "商品の削除に失敗しました" });
     }
 }
--- backend/services/practice/practiceItemService.ts
+++ backend/services/practice/practiceItemService.ts
@@ -2,72 +2,61 @@ import type { ItemInfoTypes } from "../../../shared/types/ItemInfoTypes.js";
 import * as itemRepository from "../../repositories/itemRepository.js";
 
 export const addItem = async (item: ItemInfoTypes) => {
-    const id = itemRepository.addItem(item);
+    const id = await itemRepository.addItem(item);
 
     if (!id) {
-        throw new Error("商品を追加できませんでした");
+        throw new Error("正しいidを取得できませんでした");
     }
 
-    return {
-        ...item,
-        id,
-    };
+    return { ...item, id };
 }
 
 export const getItems = async () => {
-    const [rows] = await itemRepository.getItems();
+    const [items] = await itemRepository.getItems();
 
-    if (!rows) {
-        throw new Error("商品を取得できませんでした");
-    }
-
-    const items = rows.map((row) => ({
-        id: row.id,
-        itemName: row.item_name,
-        description: row.description,
-        quantity: row.quantity,
-        unit: row.unit,
-        unitPrice: row.unit_price
+    const result = items.map((item) => ({
+        id: item.id,
+        itemName: item.item_name,
+        quantity: item.quantity,
+        unit: item.unit,
+        unitPrice: item.unit_price,
+        description: item.description
     }))
 
-    return items;
+    return result;
 }
 
 export const searchItem = async (keyword: string) => {
     const [items] = await itemRepository.searchItem(keyword);
 
-    if (!items) {
-        throw new Error("商品を取得できませんでした");
-    }
-
-    const item = items.map((i) => ({
-        id: i.id,
-        itemName: i.item_name,
-        description: i.description,
-        quantity: i.quantity,
-        unit: i.unit,
-        unitPrice: i.unit_price,
+    const result = items.map((item) => ({
+        id: item.id,
+        itemName: item.item_name,
+        quantity: item.quantity,
+        unit: item.unit,
+        unitPrice: item.unit_price,
+        description: item.description
     }))
 
-    return item;
+    return result;
 }
 
 export const updateItem = async (id: number, item: ItemInfoTypes) => {
-    const updatedItem = await itemRepository.updateItem(id, item);
+    const updated = await itemRepository.updateItem(id, item);
 
-    if (!updatedItem) {
-        throw new Error("更新に失敗しました");
+    if (!updated) {
+        return null;
     }
 
-    return updatedItem;
+    return updated;
 }
 
 export const deleteItem = async (id: number) => {
-    const [result] = await itemRepository.deleteItem(id);
+    const deleted = await itemRepository.deleteItem(id);
 
-    if (result.affectedRows === 0) {
-        return false;
+    if (!deleted) {
+        return null;
     }
 
-    return true;
+    return deleted;
 }
