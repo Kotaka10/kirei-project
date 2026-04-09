fa21162d0587039965dc43a22d331bcde7b401f7
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-04-09 09:12:33 +0900

    feat: ファイルアップロード/取得にまつわる関数追加

--- frontend/src/features/practice/hooks/usePictureBlob.tsx
+++ frontend/src/features/practice/hooks/usePictureBlob.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
+import type { UploadByBlobResponse } from "../../types/UploadResponse";
+import { useParams } from "react-router-dom";
 
 export default function usePictureBlob() {
     const [originalUrl, setOriginalUrl] = useState("");
     const [processedUrl, setProcessedUrl] = useState("");
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [result, setResult] = useState("");
+
+    const { id } = useParams();
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
@@ -43,9 +49,50 @@ export default function usePictureBlob() {
         }
     };
 
+    const handleUpload = async () => {
+        if (!selectedFile) {
+            setResult("ファイルを選択してください");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("file", selectedFile);
+
+        try {
+            const res = await fetch("http://localhost:3000/upload-blob", {
+                method: "POST",
+                body: formData,
+            });
+
+            const data: UploadByBlobResponse = await res.json();
+            setResult(JSON.stringify(data, null, 2));
+        } catch (err) {
+            console.error(err);
+            setResult("アップロードアップロードに失敗しました");
+        }
+    }
+
+    const fetchFileById = async () => {
+        try {
+            const res = await fetch(`http://localhost:3000/upload-blob/${id}`);
+            if (!res.ok) {
+                throw new Error("ファイルが見つかりませんでした");
+            }
+
+            await res.json();
+        } catch (err) {
+            console.error(err);
+            setResult("ファイルの取得に失敗しました");
+        }
+    }
+
     return {
         originalUrl,
         processedUrl,
-        handleChange
+        handleChange,
+        handleUpload,
+        result,
+        setSelectedFile,
+        id
     }
 }
