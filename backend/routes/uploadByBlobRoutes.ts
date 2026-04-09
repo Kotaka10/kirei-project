b64dbf37264afab9c0a44bcee03af6f5031325b2
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-04-09 09:08:21 +0900

    feat: ファイルアップロード/ファイル取得ルーター追加

--- backend/routes/uploadByBlobRoutes.ts
+++ backend/routes/uploadByBlobRoutes.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { uploadConfig } from "../middlewares/uploadFile.js";
-import { uploadFile } from "../controllers/uploadByBlobController.js";
+// import { uploadFile } from "../controllers/uploadByBlobController.js";
+import * as uploadByBlobController from "../controllers/uploadByBlobController.js";
 
 const router = express.Router();
 
-router.post("/", uploadConfig.single("file"), uploadFile);
+// router.post("/", uploadConfig.single("file"), uploadFile);
+router.post("/", uploadConfig.single("file"), uploadByBlobController.upload);
+router.get("/:id", uploadByBlobController.display);
 
 export default router;
