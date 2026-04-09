c5af54428036bc9620ea6a969913be4bc37a295b
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-04-09 09:13:53 +0900

    feat: レスポンス用の型を追加

--- frontend/src/features/types/UploadResponse.ts
+++ frontend/src/features/types/UploadResponse.ts
@@ -6,3 +6,14 @@ export type UploadResponse = {
     note?: string;
     parsedJson?: unknown;
 }
+
+export type UploadByBlobResponse = {
+    message: string;
+    file?: {
+        id: number;
+        filename: string;
+        mimetype: string;
+        size: number;
+        created_at: string;
+    };
+};
