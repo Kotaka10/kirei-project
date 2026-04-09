9bb043b90f05eb513432a03b3b626296f922f487
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-04-09 09:11:27 +0900

    feat:ファイルアップロードにまつわるjsxの記述追加

--- frontend/src/features/practice/PictureBlob.tsx
+++ frontend/src/features/practice/PictureBlob.tsx
@@ -5,12 +5,15 @@ export default function PictureBlob() {
         originalUrl,
         processedUrl,
         handleChange,
+        handleUpload,
+        result,
+        setSelectedFile
     } = usePictureBlob();
 
     return (
         <div className="min-h-screen bg-slate-50 p-8">
             <div className="mx-auto max-w-4xl rounded-2xl bg-white p-6 shadow">
-                <h1 className="mb-4 text-2xl font-bold">画像 Blob プレビュー</h1>
+                <h1 className="mb-4 text-2xl font-bold">ファイル Blob</h1>
 
                 <input type="file" accept="image/*" onChange={handleChange} />
 
@@ -33,6 +36,26 @@ export default function PictureBlob() {
                         )}
                     </div>
                 </div>
+
+                <input
+                    type="file"
+                    onChange={(e) => setSelectedFile(e.target.files?.[0] ?? null)}
+                    className="my-8 block"
+                />
+                <button
+                    onClick={handleUpload}
+                    className="rounded-xl bg-black px-4 py-2 text-white"
+                >
+                    保存
+                </button>
+                <pre className="mt-6 overflow-auto rounded-xl bg-slate-100 p-4 text-sm">
+                    {result}
+                </pre>
+                <img
+                    src="http://localhost:3000/upload-blob/2"
+                    alt="uploaded"
+                    className="rounded-xl border mt-4 w-full h-full"
+                />
             </div>
         </div>
     )
