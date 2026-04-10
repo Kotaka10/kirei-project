5d640e52bc8493866a96c26a840cde9b11385876
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-04-10 18:03:01 +0900

    feat: メッセージに関するapi取得関数追加

---
+++ frontend/src/features/services/messageApi.ts
@@ -0,0 +1,11 @@
+import type { Message } from "../../../../shared/types/MessageTypes";
+
+export const fetchMessages = async (): Promise<Message[]> => {
+    const res = await fetch("http://localhost:3000/messages");
+
+    if (!res.ok) {
+        throw new Error("メッセージ取得に失敗しました");
+    }
+
+    return res.json();
+}
