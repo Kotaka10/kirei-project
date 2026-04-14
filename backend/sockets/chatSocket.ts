5322ebc9d1f689e399c56dabe7a0c27395a3b03f
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-04-14 17:23:56 +0900

    refactor: コメント追加

--- backend/sockets/chatSocket.ts
+++ backend/sockets/chatSocket.ts
@@ -2,14 +2,14 @@ import type { Server } from "socket.io";
 import { messageService } from "../services/messageService.js";
 
 export const registerChatSocket = (io: Server) => {
-    io.on("connection", (socket) => {
-        socket.on("send_message", ({ userName, text }) => {
+    io.on("connection", (socket) => {// クライアントが接続したときに実行される socket = 「そのクライアント専用の通信口」
+        socket.on("send_message", ({ userName, text }) => {// クライアントからこれが来る想定 → socket.emit("send_message", { userName, text }); socket.emit → その人だけに送信
             try {
                 const saved = messageService.createMessage(userName, text);
 
-                io.emit("receive_message", saved);
+                io.emit("receive_message", saved); // 接続している全クライアントに送信
             } catch (e) {
-                socket.emit("chat_error", { message: "error" });
+                socket.emit("chat_error", { message: "error" }); //何か失敗したら 👉 送信した本人だけにエラー返す
             }
         })
     })
