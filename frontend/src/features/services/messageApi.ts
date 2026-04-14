aee5248a545155fb39d7b271c50027d8578dba56
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-04-14 17:24:53 +0900

    refactor: fetch時のパス変更

--- frontend/src/features/services/messageApi.ts
+++ frontend/src/features/services/messageApi.ts
@@ -1,7 +1,7 @@
 import type { Message } from "../../../../shared/types/MessageTypes";
 
 export const fetchMessages = async (): Promise<Message[]> => {
-    const res = await fetch("http://localhost:3000/messages");
+    const res = await fetch("http://localhost:3000/api/messages");
 
     if (!res.ok) {
         throw new Error("メッセージ取得に失敗しました");
