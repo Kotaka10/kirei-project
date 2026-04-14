37adf84853e553b815cc37d12e704afa75dc2f6e
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-04-14 17:30:29 +0900

    refactor: useRefをつかって値を保持して利用するよう修正/コメント追加

--- frontend/src/features/chat/hooks/useChat.tsx
+++ frontend/src/features/chat/hooks/useChat.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { io, type Socket } from "socket.io-client";
 import type { Message } from "../../../../../shared/types/MessageTypes";
 import { fetchMessages } from "../../services/messageApi";
-import { socket } from "../../services/socket";
-
 
 export default function useChat() {
     const [messages, setMessages] = useState<Message[]>([]);
     const [userName, setUserName] = useState("");
     const [text, setText] = useState("");
+    const socketRef = useRef<Socket | null>(null);
 
     useEffect(() => {
         const init = async () => {
@@ -23,27 +23,31 @@ export default function useChat() {
     }, []);
 
     useEffect(() => {
-        const handleReceiveMessage = (message: Message) => {
+        const socket = io("http://localhost:3000"); //サーバーに接続
+        socketRef.current = socket; //他の場所でも使えるように保持（再レンダリングでも消えない）
+
+        const handleReceiveMessage = (message: Message) => { //メッセージが来たら配列に追加
             setMessages((prev) => [...prev, message]);
         };
 
-        const handleChatError = (error: { message: string }) => {
+        const handleChatError = (error: { message: string }) => {//　サーバーのこれに対応 → socket.emit("chat_error", { message: "error" });
             alert(error.message);
         };
 
-        socket.on("receive_message", handleReceiveMessage);
-        socket.on("chat_error", handleChatError);
+        socket.on("receive_message", handleReceiveMessage); //サーバーからのイベントを待ち受ける
+        socket.on("chat_error", handleChatError);           //サーバーからのイベントを待ち受ける
 
         return () => {
-            socket.off("receive_message", handleReceiveMessage);
+            socket.off("receive_message", handleReceiveMessage); //socket.off(...) → イベントリスナーを削除
             socket.off("chat_error", handleChatError);
+            socket.disconnect(); //サーバーとの接続を閉じる
         };
     }, []);
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
 
-        socket.emit("send_message", {
+        socketRef.current?.emit("send_message", {
             userName,
             text,
         });
