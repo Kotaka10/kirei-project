985c1eff33cb859645317d10d8cc0c0739a2fc5a
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-05-01 14:09:07 +0900

    refactor: エラー解消に伴い記述変更

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,17 @@ export default defineConfig({
   server: {
     allowedHosts: [
       'waviness-unsightly-freely.ngrok-free.dev',
-    ]
+    ],
+    proxy: {
+      '/socket.io': {
+        target: 'http://backend:3000',
+        ws: true,
+        changeOrigin: true,
+      },
+      '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+      },
+    },
   },
 })
