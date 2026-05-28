a1f97f3d0392149658148a2896ccfe570470af2e
古宮隆宏 <komiya.t@cleanrevolution.jp>
2026-05-28 12:05:50 +0900

    feat：資材ハンドラーのexport追加

--- backend/tools/handlers.ts
+++ backend/tools/handlers.ts
@@ -3,3 +3,4 @@ export * from "./handlers/staffHandlers.js";
 export * from "./handlers/skillHandlers.js";
 export * from "./handlers/bookingHandlers.js";
 export * from "./handlers/salesHandlers.js";
+export * from "./handlers/materialsHandlers.js";
